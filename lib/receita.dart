88558d5eb08c3df4e59074e22a232580bab19c28
Maurício Vieira <mauriciomvdr@hotmail.com>
2020-08-29 11:44:52 -0300

    Adicionando atributo image

--- lib/receita.dart
+++ lib/receita.dart
@@ -3,11 +3,18 @@ class Receita {
   String modoDeFazer;
   String rendimento;
   String ingredientes;
+  String tempoPreparo;
+  String imagem;
 
-  Receita(nomeReceita, ingredientes, modoDeFazer, rendimento) {
+  Receita(nomeReceita, ingredientes, modoDeFazer, rendimento, tempoPreparo) {
     this.nomeReceita = nomeReceita;
     this.ingredientes = ingredientes;
     this.modoDeFazer = modoDeFazer;
     this.rendimento = rendimento;
+    this.tempoPreparo = tempoPreparo;
+  }
+
+  void setImage(String fileName) {
+    this.imagem = fileName;
   }
 }
