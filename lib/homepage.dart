dc9bb868016fe8a5e70f0f4afb561d9f0016043f
Maurício Vieira <mauriciomvdr@hotmail.com>
2020-08-29 22:23:37 -0300

    ajustando acoes do botao

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,7 +16,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage>
-  with SingleTickerProviderStateMixin {
+    with SingleTickerProviderStateMixin {
   Usuario user;
 
   AnimationController animationController;
@@ -63,138 +63,139 @@ class _MyHomePageState extends State<MyHomePage>
     // TODO: ajutar os dados do usuario
     print(user);
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: <Widget>[
-            Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("imgs/icon.png"),
-                  fit: BoxFit.fitHeight
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("imgs/icon.png"),
+                      fit: BoxFit.fitHeight),
                 ),
               ),
-            ),
-            SizedBox(width: 5),
-            Text("iCook"),
-          ],
+              SizedBox(width: 5),
+              Text("iCook"),
+            ],
+          ),
+          backgroundColor: Colors.black54,
         ),
-        backgroundColor: Colors.black54,
-      ),
-      drawer: MainDrawerPage(user: user, auth: widget.auth),
-      body: Container(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              color: Colors.grey[300],
-              child: ListView(
-                children: <Widget>[
-                  RecipeTile(),
-                  RecipeTile(),
-                  RecipeTile(),
-                  RecipeTile(),
-                ],
+        drawer: MainDrawerPage(user: user, auth: widget.auth),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.grey[300],
+                child: ListView(
+                  children: <Widget>[
+                    RecipeTile(),
+                    RecipeTile(),
+                    RecipeTile(),
+                    RecipeTile(),
+                  ],
+                ),
               ),
-            ),
-            Positioned(
-              right: 30,
-              bottom: 30,
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: <Widget>[
-                  IgnorePointer(
-                    child: Container(
-                      color: Colors.white.withOpacity(0.100), // comment or change to transparent color
-                      height: 150.0,
-                      width: 150.0,
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: Offset.fromDirection(getRadiansFromDegree(270),
-                        degOneTranslationAnimation.value * 100),
-                    child: Transform(
-                      transform: Matrix4.rotationZ(
-                          getRadiansFromDegree(rotationAnimation.value))
-                        ..scale(degOneTranslationAnimation.value),
-                      alignment: Alignment.center,
-                      child: CircularButton(
-                        color: Colors.green,
-                        width: 50,
-                        height: 50,
-                        icon: Icon(
-                          Icons.add,
-                          color: Colors.white,
+              Positioned(
+                  right: 30,
+                  bottom: 30,
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: <Widget>[
+                      IgnorePointer(
+                        child: Container(
+                          color: Colors.white.withOpacity(
+                              0.100), // comment or change to transparent color
+                          height: 150.0,
+                          width: 150.0,
                         ),
-                        onClick: () {
-                          print('First Button');
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    CadastrarReceitasPage()),
-                          );
-                        },
                       ),
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: Offset.fromDirection(getRadiansFromDegree(180),
-                        degTwoTranslationAnimation.value * 100),
-                    child: Transform(
-                      transform: Matrix4.rotationZ(
-                          getRadiansFromDegree(rotationAnimation.value))
-                        ..scale(degTwoTranslationAnimation.value),
-                      alignment: Alignment.center,
-                      child: CircularButton(
-                        color: Colors.black,
-                        width: 50,
-                        height: 50,
-                        icon: Icon(
-                          Icons.list,
-                          color: Colors.white,
+                      Transform.translate(
+                        offset: Offset.fromDirection(getRadiansFromDegree(270),
+                            degOneTranslationAnimation.value * 100),
+                        child: Transform(
+                          transform: Matrix4.rotationZ(
+                              getRadiansFromDegree(rotationAnimation.value))
+                            ..scale(degOneTranslationAnimation.value),
+                          alignment: Alignment.center,
+                          child: CircularButton(
+                            color: Colors.green,
+                            width: 50,
+                            height: 50,
+                            icon: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                            onClick: () {
+                              print('First Button');
+                              animationController.reverse();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        CadastrarReceitasPage()),
+                              );
+                            },
+                          ),
                         ),
-                        onClick: () {
-                          print('Third Button');
-                        },
                       ),
-                    ),
-                  ),
-                  Transform(
-                    transform: Matrix4.rotationZ(
-                        getRadiansFromDegree(rotationAnimation.value)),
-                    alignment: Alignment.center,
-                    child: CircularButton(
-                      color: Colors.red,
-                      width: 60,
-                      height: 60,
-                      icon: Icon(
-                        Icons.menu,
-                        color: Colors.white,
+                      Transform.translate(
+                        offset: Offset.fromDirection(getRadiansFromDegree(180),
+                            degTwoTranslationAnimation.value * 100),
+                        child: Transform(
+                          transform: Matrix4.rotationZ(
+                              getRadiansFromDegree(rotationAnimation.value))
+                            ..scale(degTwoTranslationAnimation.value),
+                          alignment: Alignment.center,
+                          child: CircularButton(
+                            color: Colors.black,
+                            width: 50,
+                            height: 50,
+                            icon: Icon(
+                              Icons.list,
+                              color: Colors.white,
+                            ),
+                            onClick: () {
+                              print('Third Button');
+                              animationController.reverse();
+                            },
+                          ),
+                        ),
                       ),
-                      onClick: () {
-                        if (animationController.isCompleted) {
-                          animationController.reverse();
-                        } else {
-                          animationController.forward();
-                        }
-                      },
-                    ),
-                  )
-                ],
-              )
-            ),
-          ],
-        ),
-      )
-      //ListView.builder(
-      //  itemCount: items.count,
-      //  itemBuilder: (ctx, i) => ItemTile(items.byIndex(i)),
-      //),
-    );
+                      Transform(
+                        transform: Matrix4.rotationZ(
+                            getRadiansFromDegree(rotationAnimation.value)),
+                        alignment: Alignment.center,
+                        child: CircularButton(
+                          color: Colors.red,
+                          width: 60,
+                          height: 60,
+                          icon: Icon(
+                            Icons.menu,
+                            color: Colors.white,
+                          ),
+                          onClick: () {
+                            if (animationController.isCompleted) {
+                              animationController.reverse();
+                            } else {
+                              animationController.forward();
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  )),
+            ],
+          ),
+        )
+        //ListView.builder(
+        //  itemCount: items.count,
+        //  itemBuilder: (ctx, i) => ItemTile(items.byIndex(i)),
+        //),
+        );
   }
 }
